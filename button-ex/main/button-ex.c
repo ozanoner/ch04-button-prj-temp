4031f7b7e32359c1de3580476c86c2fa8a9fb25f
Ozan Oner <ozan.oner@gmail.com>
2026-07-07 11:15:07 +0000

    feat: add button clicks

---
+++ button-ex/main/button-ex.c
@@ -0,0 +1,39 @@
+
+#include <esp_log.h>
+#include <esp_err.h>
+#include "iot_button.h"
+#include "button_gpio.h"
+
+#define TAG "app"
+#define APP_BUTTON_GPIO 0
+
+static button_handle_t app_button = NULL;
+
+static void app_single_click_cb(void *button_handle, void *usr_data)
+{
+    ESP_LOGI(TAG, "single-click");
+}
+
+static void app_double_click_cb(void *button_handle, void *usr_data)
+{
+    ESP_LOGI(TAG, "double-click");
+}
+
+static void app_long_press_cb(void *button_handle, void *usr_data)
+{
+    ESP_LOGI(TAG, "long-press");
+}
+
+void app_main(void)
+{
+    const button_config_t button_cfg = {0};
+
+    const button_gpio_config_t gpio_cfg = {
+        .gpio_num = APP_BUTTON_GPIO};
+
+    ESP_ERROR_CHECK(iot_button_new_gpio_device(&button_cfg, &gpio_cfg, &app_button));
+
+    ESP_ERROR_CHECK(iot_button_register_cb(app_button, BUTTON_SINGLE_CLICK, NULL, app_single_click_cb, NULL));
+    ESP_ERROR_CHECK(iot_button_register_cb(app_button, BUTTON_DOUBLE_CLICK, NULL, app_double_click_cb, NULL));
+    ESP_ERROR_CHECK(iot_button_register_cb(app_button, BUTTON_LONG_PRESS_START, NULL, app_long_press_cb, NULL));
+}
